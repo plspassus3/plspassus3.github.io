23ff84ba67dc9b24945f31150ddb5191aac24618
plspassus3 <plspassus3@gmail.com>
2017-02-04 16:29:58 -0500

    Add files via upload

--- space_shooter_part_five.js
+++ space_shooter_part_five.js
@@ -780,7 +780,7 @@ function Game() {
 		var x = 100;
 		var y = -height;
 		var spacer = y * 1.5;
-		for (var i = 1; i <= 18; i++) {
+		for (var i = 1; i <= 36; i++) {
 			this.enemyPool.get(x,y,2);
 			x += width + 25;
 			if (i % 6 == 0) {
